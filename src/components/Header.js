56e079281243be8ab387217a3962a68b75ff0604
Michael John Sabellano <168133613+M23jJohn@users.noreply.github.com>
2024-08-31 19:41:00 +0800

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,27 @@
 import React from 'react';
 
-export default function Header({ comname }) { // Fixed the prop name here
-const handlePrint = () => {
+export default function Header({ comname }) {
+  const handlePrint = () => {
     window.print();
   };
+
   return (
     <div>
+      <style>
+        {`
+          @media print {
+            .no-print {
+              display: none !important;
+            }
+          }
+        `}
+      </style>
       <header className="py-6 px-8">
         <h1 className="text-sm font-bold text-left mb-6 text-black">{comname}</h1>
-        <ul className="flex justify-center space-x-4">
+        <ul className="flex justify-center space-x-4 no-print">
           <li>
             <button
-              onClick={handlePrint} // Using the correct prop name
+              onClick={handlePrint}
               className="px-4 py-2 bg-white text-blue-600 rounded-lg font-semibold hover:bg-blue-50 transition-colors duration-300"
             >
               Print
