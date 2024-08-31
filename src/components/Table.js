59843309d238acf7a5fcb24098da3169af149e90
Michael John Sabellano <168133613+M23jJohn@users.noreply.github.com>
2024-08-31 16:32:58 +0800

    update src/components/Table.js

---
+++ src/components/Table.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Table() {
+  return (
+          <div className="mb-8">
+            <p className="text-gray-600">this is the table</p>
+    </div>
+  )
+}
