1f02be8c710c207e0e591d51adef54250a3adc14
Michael John Sabellano <168133613+M23jJohn@users.noreply.github.com>
2024-08-31 18:01:43 +0800

    update src/components/MainDetails.js

--- src/components/MainDetails.js
+++ src/components/MainDetails.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-export default function MainDetails() {
+export default function MainDetails({ name, address }) {
   return (
     <>
                 <section className="mb-8 text-right">
-            <h2 className="text-2xl font-semibold text-gray-800 uppercase">Your Name</h2>
-            <p className="text-gray-600">Your Address</p>
+            <h2 className="text-2xl font-semibold text-gray-800 uppercase">{name}</h2>
+            <p className="text-gray-600">{address}</p>
           </section>
     </>
   )
