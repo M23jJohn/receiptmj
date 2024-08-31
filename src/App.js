a400e9577e044dd47b97602332bcbb426ebfa311
Michael John Sabellano <168133613+M23jJohn@users.noreply.github.com>
2024-08-31 17:08:25 +0800

    update src/App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Notes from './components/Notes.js'
 import Table from './components/Table.js'
 import Date from './components/Dates.js'
@@ -8,6 +8,7 @@ import Footer from './components/Footer.js'
 import Header from './components/Header.js'
 
 export default function App() {
+  const [showInvoice, setShowInvoice] = useState(false)
   const handlePrint = () => {
     window.print();
   };
@@ -15,6 +16,7 @@ export default function App() {
   return (
     <div className="min-h-screen bg-gray-100 py-8">
       <main className="max-w-3xl mx-auto bg-white rounded-xl shadow-lg overflow-hidden">
+        {showInvoice ?
         <div className="p-8">
        <Header handlePrint={handlePrint} />
           <Details />
@@ -23,7 +25,16 @@ export default function App() {
           <Table />
           <Notes />
           <Footer />
-        </div>
+        </div> : (
+        <>
+          <div className="flex flex-col justify-center">
+          <input type="text" name="text" id="text" placeholder="Enter Your Name" autoComplete="off" />
+          <button className="bg-blue-500 py-2 px-8 rounded shadow border-2 border-blue-500 hover:bg-transparent hover:text-blue-500 font-bold transition-all duration-300">Preview Invoice</button>
+          </div>
+        </>
+        
+        )
+          }
       </main>
     </div>
   );
