715d9e9f78652b7ae184a7150ea8ab487dc24cdd
Michael John Sabellano <168133613+M23jJohn@users.noreply.github.com>
2024-08-31 18:12:07 +0800

    update src/components/ClientDetails.js

--- src/components/ClientDetails.js
+++ src/components/ClientDetails.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
-export default function ClientDetails() {
+export default function ClientDetails({ cname, caddress}) {
   return (
-    <div>          <section className="mb-8">
-            <h2 className="text-3xl font-bold text-gray-800 uppercase">Client's Name</h2>
-            <p className="text-gray-600">Client's Address</p>
+    <div>       
+    <section className="mb-8">
+            <h2 className="text-3xl font-bold text-gray-800 uppercase">cname</h2>
+            <p className="text-gray-600">caddress</p>
           </section>
           </div>
   )
