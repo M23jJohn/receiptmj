2203a8979aadc2610c5c31fe2249cc578e1173bf
Michael John Sabellano <168133613+M23jJohn@users.noreply.github.com>
2024-08-31 16:35:24 +0800

    update src/components/Dates.js

---
+++ src/components/Dates.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function Dates() {
+  return (
+    <div>          <article className="mb-8 flex items-end justify-end">
+            <ul className="space-y-2">
+              <li>
+                <span className="font-bold text-gray-700">
+                Invoice Number:
+                </span>
+              </li>
+              <li>
+                <span className="font-bold text-gray-700">
+                Invoiced Date:
+                </span>
+              </li>
+            </ul>
+          </article></div>
+  )
+}
