71c3e36241d0fc055b5a1c1eefed61bb3638d247
Michael John Sabellano <168133613+M23jJohn@users.noreply.github.com>
2024-08-31 16:29:13 +0800

    update src/components/Notes.js

---
+++ src/components/Notes.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Notes() {
+  return (
+    <div>
+           <section className="mb-8">
+            <p className="text-gray-600">Notes to the Client...</p>
+          </section>
+    </div>
+  )
+}
