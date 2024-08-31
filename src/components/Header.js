529b2b5faf515e55fdefc6ed3c376683c0ce77f1
Michael John Sabellano <168133613+M23jJohn@users.noreply.github.com>
2024-08-31 19:20:11 +0800

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
-export default function Header({ handlePrint }) { // Fixed the prop name here
+export default function Header() { // Fixed the prop name here
   return (
+const handlePrint = () => {
+    window.print();
+  };
     <div>
       <header className="text-white py-6 px-8">
         <h1 className="text-4xl font-bold text-center mb-6">Invoice Maker</h1>
