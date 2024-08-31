ce3e8df6115ea80d609ddf1bf8f2bd663f300d3c
Michael John Sabellano <168133613+M23jJohn@users.noreply.github.com>
2024-08-31 16:27:25 +0800

    update src/components/Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div>
+           <footer className="border-t pt-4">
+            <ul className="flex flex-wrap items-center justify-between text-sm text-gray-600">
+              <li>
+                <span>
+                Your Name
+                </span>
+              </li>
+              <li>
+                <span>
+                Your Email
+                </span>
+              </li>
+              <li>
+                <span>
+                Phone Number
+                </span>
+              </li>
+            </ul>
+          </footer>
+    </div>
+  )
+}
