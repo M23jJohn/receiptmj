567d6c7050954544e7c0db8d5a10daa0e0a40e57
Michael John Sabellano <168133613+M23jJohn@users.noreply.github.com>
2024-08-31 16:27:16 +0800

    update src/components/MainDetails.js

--- src/components/MainDetails.js
+++ src/components/MainDetails.js
@@ -1,25 +1,7 @@
-export default function Footer() {
+import React from 'react'
+
+export default function MainDetails() {
   return (
-    <>
-     <footer className="border-t pt-4">
-            <ul className="flex flex-wrap items-center justify-between text-sm text-gray-600">
-              <li>
-                <span>
-                Your Name
-                </span>
-              </li>
-              <li>
-                <span>
-                Your Email
-                </span>
-              </li>
-              <li>
-                <span>
-                Phone Number
-                </span>
-              </li>
-            </ul>
-          </footer>
-    </>
-    )
+    <></>
+  )
 }
