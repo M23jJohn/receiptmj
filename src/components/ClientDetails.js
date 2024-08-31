67f6b71d0a5b1936a2b09f45042db01fc925f24e
Michael John Sabellano <168133613+M23jJohn@users.noreply.github.com>
2024-08-31 18:52:50 +0800

    update src/components/ClientDetails.js

--- src/components/ClientDetails.js
+++ src/components/ClientDetails.js
@@ -4,8 +4,8 @@ export default function ClientDetails({ cname, caddress}) {
   return (
     <div>       
     <section className="mb-8">
-            <h2 className="text-3xl font-bold text-gray-800 uppercase">cname</h2>
-            <p className="text-gray-600">caddress</p>
+            <h2 className="text-3xl font-bold text-gray-800 uppercase">{cname}</h2>
+            <p className="text-gray-600">{caddress}</p>
           </section>
           </div>
   )
