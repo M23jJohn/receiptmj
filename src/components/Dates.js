313f0eb308af4bca9b2867e7d55ba397bdf330aa
Michael John Sabellano <168133613+M23jJohn@users.noreply.github.com>
2024-08-31 18:10:18 +0800

    update src/components/Dates.js

--- src/components/Dates.js
+++ src/components/Dates.js
@@ -1,18 +1,21 @@
 import React from 'react'
 
-export default function Dates() {
+export default function Dates({ indate, innum }) {
   return (
-    <div>          <article className="mb-8 flex items-end justify-end">
+    <div>        
+    <article className="mb-8 flex items-end justify-end">
             <ul className="space-y-2">
               <li>
                 <span className="font-bold text-gray-700">
                 Invoice Number:
                 </span>
+                {innum}
               </li>
               <li>
                 <span className="font-bold text-gray-700">
                 Invoiced Date:
                 </span>
+                {indate}
               </li>
             </ul>
           </article>
